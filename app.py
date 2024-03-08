f52670bf3aaef5eef2b4ee1d1a8ae4389054f260
Axel Depoitre <105279908+Axelitoooo@users.noreply.github.com>
2024-03-08 14:23:43 +0100

    V0.3
    add login

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 import json
 import os
-
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from functools import wraps
 
 app = Flask(__name__)
+# Utilisateurs autorisés (vous pouvez remplacer cela par une base de données utilisateur)
+AUTHORIZED_USERS = {'sadish': 'sadish1'}
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in request.cookies or 'password' not in request.cookies:
+            return redirect(url_for('login'))
+        elif request.cookies['username'] not in AUTHORIZED_USERS or request.cookies['password'] != AUTHORIZED_USERS[
+            request.cookies['username']]:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in AUTHORIZED_USERS and password == AUTHORIZED_USERS[username]:
+            response = redirect(url_for('results'))
+            response.set_cookie('username', username)
+            response.set_cookie('password', password)
+            return response
+    return render_template('login.html')
 
 
 @app.route('/results', methods=['GET', 'POST'])
-def show_results():
-    data_folder = os.path.join(os.path.dirname(__file__), 'data')
+@login_required
+def results():
+    data_folder = os.path.join(os.path.dirname(__file__), 'data')  # Importation correcte de 'os'
     if not os.path.exists(data_folder):
         os.makedirs(data_folder)
     files = [f for f in os.listdir(data_folder) if f.endswith('.json')]
@@ -25,6 +54,7 @@ def show_results():
 
 
 @app.route('/check-update', methods=['GET'])
+@login_required
 def check_update():
     data_folder = os.path.join(os.path.dirname(__file__), 'data')
     most_recent_file = max([os.path.join(data_folder, f) for f in os.listdir(data_folder) if f.endswith('.json')],
@@ -33,6 +63,14 @@ def check_update():
         return jsonify({"update": True, "file": os.path.basename(most_recent_file),
                         "timestamp": os.path.getmtime(most_recent_file)})
     return jsonify({"update": False})
+
+
+@app.route('/logout')
+def logout():
+    response = redirect(url_for('login'))
+    response.delete_cookie('username')
+    response.delete_cookie('password')
+    return response
 
 
 if __name__ == '__main__':
