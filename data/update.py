703c8fd868bdefaef559c53be06e666bb1f8c4e7
Axel Depoitre <105279908+Axelitoooo@users.noreply.github.com>
2024-03-08 15:53:31 +0100

    V0.3

--- data/update.py
+++ data/update.py
@@ -2,12 +2,26 @@ import subprocess
 import requests
 import time
 
+# Assurez-vous de remplacer 'your_personal_access_token' par votre token d'accès personnel GitHub
+github_token = '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'
+
 
 def check_for_updates(github_repo):
-    api_url = f"https://api.github.com/repos/{github_repo}/commits"
-    response = requests.get(api_url)
-    commits = response.json()
-    return commits
+    api_url = f"https://api.github.com/repos/{github_repo}/commits?per_page=1"
+    headers = {'Authorization': f'token {github_token}'}
+    response = requests.get(api_url, headers=headers)
+    print(f"Vérification des mises à jour pour {github_repo}")
+    if response.status_code == 200:
+        commits = response.json()
+        if commits and isinstance(commits, list) and len(commits) > 0:
+            last_commit_sha = commits[0]['sha']
+            print(f"Dernier commit SHA: {last_commit_sha}")
+            return last_commit_sha
+        else:
+            print("Pas de nouveaux commits trouvés.")
+    else:
+        print(f"Erreur lors de la récupération des commits. Statut HTTP: {response.status_code}")
+    return None
 
 
 def pull_changes():
@@ -19,20 +33,22 @@ def main():
     last_sha = None
 
     while True:
-        commits = check_for_updates(github_repo)
+        current_sha = check_for_updates(github_repo)
 
-        if commits and (last_sha is None or commits[0]['sha'] != last_sha):
+        if current_sha and current_sha != last_sha:
             print("Des modifications ont été détectées. Voulez-vous mettre à jour ? [y/n]")
             choice = input()
 
             if choice.lower() == 'y':
                 pull_changes()
                 print("Mise à jour effectuée.")
-                last_sha = commits[0]['sha']
+                last_sha = current_sha
             else:
                 print("Mise à jour ignorée.")
+        else:
+            print("Aucune mise à jour disponible ou erreur lors de la vérification des mises à jour.")
 
-        time.sleep(600)  # Vérifier les mises à jour toutes les 10 minutes
+        time.sleep(60)  # Vérifier les mises à jour toutes les 10 minutes
 
 
 if __name__ == "__main__":
